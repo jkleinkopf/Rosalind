3cc42a709d7bf081978d8955d598e668178e05ac
Joseph Kleinkopf <jkleinkopf@users.noreply.github.com>
2016-12-17 12:05:35 -0800

    Update FIB.py

--- FIB.py
+++ FIB.py
@@ -1,12 +1,12 @@
-n = 33
-k = 4
+n = 33 #number of months
+k = 4 #number of offspring pairs for every mature rabbit pair
 
-e = [0] * n
-e[0] = 1
+e = [0] * n #create a list of length n, months. Value of e[n] is number of rabbit pairs living in that month.
+
+e[0] = 1 #set e[0] and e[1] = 1 for the starting pair of rabbits
 e[1] = 1
 
-for i in range(n-2):
+for i in range(n-2): #3rd month is equal to 2nd month + 1st month * 3. 
 	e[i+2] = ( e[i+1] + k*e[i] )
-	
-print e
+
 print e[n-1]
